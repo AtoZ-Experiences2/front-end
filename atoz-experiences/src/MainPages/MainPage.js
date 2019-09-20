3832d097e2ff7f3aab2c2b3afe4a1364bb3b38f7
spettigrew <sara.pettigrew5@gmail.com>
2019-09-20 15:32:15 -0600

    main page set up with actitivies listed and mapped over

--- atoz-experiences/src/MainPages/MainPage.js
+++ atoz-experiences/src/MainPages/MainPage.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import TopExp from '../ChildrenComponents/TopExp';
 import AllExperienceCard from '../ChildrenComponents/AllExperiencesCard'
+import AllExperiencesCard from '../ChildrenComponents/AllExperiencesCard';
 
 export default function MainPage () {
+  const [activities, setActivities] = useState ([])
+
+  useEffect(() => {
+    const getActivityList = () => {
+      axios
+      .get("")
+      .then(resp => {
+        setActivities(resp.data.results);
+        console.log(resp.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    }
+
+    getActivityList();
+  }, []);
 
   return (
     <div>
-      
+      {activities.map(activity => (
+        <AllExperiencesCard key={activity.category} activity={activity} city={activity.city} topic={activity.topic} start_date={activity.start_date} facet_values={activity.facet_values} />
+      ))}
     </div>
   )
 }
