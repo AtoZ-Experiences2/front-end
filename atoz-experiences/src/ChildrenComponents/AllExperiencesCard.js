2e90007b68e8650ac35122ff003f822c81d85fea
spettigrew <sara.pettigrew5@gmail.com>
2019-09-20 15:42:43 -0600

    AllExperiencesCard set up with activity, date, location, price

--- atoz-experiences/src/ChildrenComponents/AllExperiencesCard.js
+++ atoz-experiences/src/ChildrenComponents/AllExperiencesCard.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-export default function AllExperiencesCard () {
+export default function AllExperiencesCard (props) {
+  const { topic, image, city, activity, start_date, facet_values }= props.activity;
 
   return (
-    <div>
-    </div>
+    <>
+    <Img src={image} alt={activity} />
+    <AllExperiencesCard>Experience You Want: {activity} Category: {topic} Location: {city} Date(s): {start_date} Price: {facet_values} /> 
+    </>
   )
 }
