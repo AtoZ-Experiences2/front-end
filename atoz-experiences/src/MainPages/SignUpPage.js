d0fca83ec8fa9693468585b1c3ebe3407f1e513c
spettigrew <sara.pettigrew5@gmail.com>
2019-09-20 19:44:30 -0600

    name and email changed from username, password.

--- atoz-experiences/src/MainPages/SignUpPage.js
+++ atoz-experiences/src/MainPages/SignUpPage.js
@@ -21,19 +21,19 @@ function UserForm({ errors, touched, status }) {
      <Form>
        <div>
         {touched.username && errors.username && <p>{errors.username} </p>}
-        <Field type="text" name="username" placeholder="Username" />
+        <Field type="text" name="name" placeholder="Name" />
        </div>
        <div>
-          {touched.password && errors.password && <p>{errors.password}</p>}
-          <Field type="password" name="password" placeholder="Password" />
+          {touched.email && errors.email && <p>{errors.email}</p>}
+          <Field type="email" name="email" placeholder="Email" />
         </div>
         <button>Register</button>
      </Form>
       {signUp.map((items, index) => {
         return (
            <div key={index}>
-           <h2>{items.username}</h2>
-           <p>{items.password}</p>
+           <h2>{items.name}</h2>
+           <p>{items.email}</p>
            </div>
         )
       })}
@@ -42,24 +42,24 @@ function UserForm({ errors, touched, status }) {
 }
 
 const FormikNewUser = withFormik({
-  mapPropsToValues({ username, password }) {
+  mapPropsToValues({ name, email }) {
     return {
-      username: username || "",
-      password: password || ""
+      name: name || "",
+      email: email || ""
     };
   },
 
   validationSchema: Yup.object().shape({
-    username: Yup.string()
-      .required("Username Required"),
-      password: Yup.string()
-      .min(8, "Password must be 8 or more characters")
-      .required("Password required")
+    name: Yup.string()
+      .required("Name Required"),
+    email: Yup.string()
+      .email("Email not valid")
+      .required("Email required"),
   }),
 
   handleSubmit(values, { resetForm, setErrors, setSubmitting, setStatus }) {
-    if (values.username === "") {
-      setErrors({ username: "That username is already taken" });
+    if (values.email === "") {
+      setErrors({ email: "That email is already taken" });
     } else {
       axios
         .post("https://reqres.in/api/users", values)
