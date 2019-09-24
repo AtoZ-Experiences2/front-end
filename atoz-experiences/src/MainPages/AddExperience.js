e22d64424785b5cff54a41628833c54f9a6040da
spettigrew <sara.pettigrew5@gmail.com>
2019-09-23 19:39:53 -0600

    add experience page set up

--- atoz-experiences/src/MainPages/AddExperience.js
+++ atoz-experiences/src/MainPages/AddExperience.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AddEventForm from '../ChildrenComponents/AddExpPageComponents/AddEventForm'
 
-export default function AddExperience () {
+export default function AddExperience({ touched, errors }) {
 
   return(
     <div>
-      
+      <h4>What experience would you like to add?</h4>
+      {touched.name && errors.name && <p>{errors.name}</p>}
+      <Field type="text" name="name" placeholder="New Experience" />
     </div>
   )
 }
